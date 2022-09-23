9ecdad93f2a8b67d55941ab678bde529150f0e83
Filip Gurak <filip.gurak999@gmail.com>
2022-09-23 23:17:57 +0200

    cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import { Store } from '../utils/Store';
 
 function Layout({ title, children }) {
+  const { state } = useContext(Store);
+
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
   return (
     <>
       <Head>
@@ -17,7 +28,14 @@ function Layout({ title, children }) {
             </Link>
             <div>
               <Link href='/cart'>
-                <a className='p-2'>Cart</a>
+                <a className='p-2'>
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
               </Link>
               <Link href='/login'>
                 <a className='p-2'>Login</a>
@@ -27,7 +45,7 @@ function Layout({ title, children }) {
         </header>
         <main className='container m-auto mt-4'>{children}</main>
         <footer className='flex h-10 justify-center items-center shadow-inner'>
-            <p>Copyright &copy; 2022 Amazona</p>
+          <p>Copyright &copy; 2022 Amazona</p>
         </footer>
       </div>
     </>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,18 +1,42 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
+
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((product) => product.slug === slug);
 
+  const router = useRouter();
+
   if (!product) {
     return <div>Product not found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find(
+      (cartItem) => cartItem.slug === product.slug
+    );
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry, product is out of stock');
+      return;
+    }
+
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: quantity },
+    });
+    router.push('/cart');
+  };
+
   return (
     <Layout title={product.name}>
       <div className='py-2'>
@@ -51,7 +75,12 @@ function ProductScreen() {
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
             </div>
-            <button className='primary-button w-full'>Add to cart</button>
+            <button
+              className='primary-button w-full'
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
